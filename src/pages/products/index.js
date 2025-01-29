d469f6ffb7b7b945bb15c45ac27a2e788a38290e
Imron-DE <imronrosyadi97@gmail.com>
2025-01-29 14:18:16 +0700

    useRef

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,75 +1,150 @@
 import Button from "@/components/atoms/Button";
 import CardProduct from "@/components/molecules/CardProduct";
-import React, { useEffect, useState } from "react";
-
-const data = [
-  {
-    id: 1,
-    image: "/images/es teler.jpg",
-    title: "Es Teler",
-    desc: "Es Teler segar dengan ",
-    price: "Rp. 100.000",
-  },
-  {
-    id: 2,
-    image: "/images/spageti.jpg",
-    title: "spageti",
-    desc: "spageti dengan bahan utama daging sapi dalam keadaan hangat dan diberi saus tomat secukupnya",
-    price: "Rp. 100.000",
-  },
-  {
-    id: 3,
-    image: "/images/esdoger.jpg",
-    title: "Es Doger",
-    desc: "Es Doger segar dengan",
-    price: "Rp. 100.000",
-  },
-];
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { data } from "@/constant/product";
+import BackToTopButton from "@/components/atoms/icons/BackToTopButton";
 
 const ProductPage = () => {
   const [username, setUsername] = useState("");
-  // sebutan variable di react
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+  const footerRef = useRef();
+  const [showBackToTop, setshowBackTOTOP] = useState(false);
+  // useref : hooks untuk membuat ref ke element DOM/fungsi untuk mengakses element DOM
 
-  // useeffect untuk menangani side effect / effect perubahan dari suatu data
   useEffect(() => {
     const getUserName = localStorage.getItem("username");
-    if (getUserName) setUsername(getUserName);
-  }, []); // defendancy array : kalo kosong untuk memastika fungsi ini dijalankan sekali saja setiapkali halaman di load
-  // kalo ada state di dalama defendancy array maka fungsi ini untuk memantau perubahan di state tersebut
 
-  // event handller untuk menjalankan fungsi logout dan menghapus data username dan password dari localstorage
+    if (getUserName) {
+      setUsername(getUserName);
+    }
 
-  function HandleLogout() {
+    setCart(JSON.parse(localStorage.getItem("cart")) || []);
+  }, []);
+
+  const handleAddToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(cart.map((item) => (item.id === id ? { ...item, qty: item.qty + 1 } : item)));
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
+  };
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      const sumTotal = cart.reduce((total, item) => {
+        const product = data.find((product) => product.id === item.id);
+        return total + product.price * item.qty;
+      }, 0);
+      setTotal(sumTotal);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+
+  function handleLogout() {
     localStorage.removeItem("username");
     localStorage.removeItem("password");
     window.location.href = "/login";
   }
 
+  useEffect(() => {
+    function handleScroll() {
+      // ambil nilai offsetTop(posisi vertical) dari elemen footer yang direferensikan oleh footerRef
+      const footerTop = footerRef.current.offsetTop; // ambil batas atas komponen
+
+      // ambil tinggi innerHeight dari object window(tinggi viewport tanpa toolbar & scrolbar)
+      const viewportHeight = window.innerHeight;
+
+      // ambil posisi scroll dari object window(posisi scroll vertical {sumbu y) dilayar )
+      const scrollPosition = window.scrollY;
+
+      // logic untuk ngecek apakah posisi scroll diayar telah mencapai element footer
+      if (scrollPosition + viewportHeight >= footerTop) {
+        setshowBackTOTOP(true);
+      } else {
+        setshowBackTOTOP(false);
+      }
+    }
+
+    // event listener untuk menjalankan fungsi handle scroll ketika halaman di scroll terjadi
+    window.addEventListener("scroll", handleScroll);
+
+    // unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [footerRef]); //jalankan side effect ini tiap kali nilai footerRef berubah
+
+  function handleBackToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
   return (
     <>
       <div className="flex justify-between items-center bg-black text-white font-bold px-5 py-4">
-        <h1 className="text-3xl font-bold mb-2">HI, {username}</h1>
-        <Button buttonClassname="bg-red-500 text-white w-24 " onClick={HandleLogout}>
+        <h1 className="text-xl">Hi, {username}</h1>
+        <Button buttonClassname={"bg-red-500 hover:bg-red-700"} onClick={handleLogout}>
           Logout
         </Button>
       </div>
-
-      <div className="flex justify-center items-center min-h-screen gap-2">
-        {/* Panggil Card Product */}
-        <CardProduct>
-          <CardProduct.Header image="/images/pizza.jpg" />
-          <CardProduct.Body title="Pizza" desc="Pizza dengan bahan utama daging sapi dalam keadaan hangat dan diberi saus tomat secukupnya" />
-          <CardProduct.Footer price="Rp. 100.000" />
-        </CardProduct>
-        {/* Rendering List : teknik untuk mmenampilkan berdasarkan beberapa data dinamis yang di simpan dalam sebuah json */}
-        {data.map((item) => (
-          <CardProduct key={item.id}>
-            <CardProduct.Header image={item.image} width={300} height={300} />
-            <CardProduct.Body title={item.title} desc={item.desc} />
-            <CardProduct.Footer price={item.price} />
-          </CardProduct>
-        ))}
+      <div className="flex px-5 py-8">
+        {/* products */}
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold text-blue-500 uppercase mb-4">Products</h1>
+          <div className="flex flex-wrap gap-4">
+            {data.map((item) => (
+              <CardProduct key={item.id}>
+                <CardProduct.Header image={item.image} />
+                <CardProduct.Body title={item.title} desc={item.desc} />
+                <CardProduct.Footer price={item.price} handleAddToCart={handleAddToCart} id={item.id} />
+              </CardProduct>
+            ))}
+          </div>
+        </div>
+        {/* cart */}
+        {cart.length > 0 && (
+          <div className="w-2/3">
+            <h1 className="text-3xl font-bold text-blue-500 mb-4 uppercase">Cart</h1>
+            <div className="flex flex-col gap-2">
+              {cart.map((item) => {
+                const datas = data.find((data) => data.id === item.id);
+                return (
+                  <div className="flex p-4 border rounded-lg" key={item.id}>
+                    <Image className="rounded" width={100} height={100} src={datas.image} alt="cart image" />
+                    <div className="flex justify-between w-full">
+                      <div className="flex flex-col justify-between ml-3">
+                        <span className="font-bold text-xl">{datas.title}</span>
+                        <span className="font-semibold">{datas.price}</span>
+                      </div>
+                      <div className="flex flex-col justify-center items-center">
+                        <span className="mb-1">Qty</span>
+                        <span className="flex justify-center items-center font-semibold p-2 border rounded-sm text-center w-10 h-10">{item.qty}</span>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex justify-between px-4 py-2 border mt-2 font-semibold rounded-lg">
+              <span>Total</span>
+              <span>{total}</span>
+            </div>
+          </div>
+        )}{" "}
       </div>
+
+      {/* footer */}
+      {showBackToTop && (
+        <div onClick={handleBackToTop} className="fixed bottom-20 right-5 bg-gradient-hover p-2 rounded-full ">
+          <BackToTopButton />
+        </div>
+      )}
+      <footer ref={footerRef} className="text-center py-4 bg-black text-white w-full">
+        All rights reserved &copy; || by Imron
+      </footer>
     </>
   );
 };
