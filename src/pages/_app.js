d13a9c1577831ddc966b030289e56464769b2961
Imron-DE <imronrosyadi97@gmail.com>
2025-01-30 17:15:02 +0700

    jotai

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,20 @@
+import { isMobileScreenAtom } from "@/atoms/atoms";
 import { setIsMobileScreen, setIsLargeScreen } from "@/redux/screenSlice/screenSlice";
 import store from "@/redux/store";
 import "@/styles/globals.css";
+import { useSetAtom } from "jotai";
 import { useEffect } from "react";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
+  const setIsMobileScreenAtom = useSetAtom(isMobileScreenAtom);
+
   useEffect(() => {
     function handleResize() {
       // dispatch : untuk mengirimkan aksi yang memicu pembaruan nilai state
       store.dispatch(setIsMobileScreen(window.innerWidth < 768));
       store.dispatch(setIsLargeScreen(window.innerWidth > 1024));
+      setIsMobileScreenAtom(window.innerWidth < 768);
     }
 
     window.addEventListener("resize", handleResize);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
+import { isMobileScreenAtom } from "@/atoms/atoms";
 import { useLogin } from "@/hooks/useLogin";
+import { useAtom } from "jotai";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
@@ -7,7 +9,10 @@ export default function Home() {
 
   const [data, setData] = useState(true);
   const username = useLogin();
-  const { isMobileScreen } = useSelector((state) => state.screen);
+  // const { isMobileScreen } = useSelector((state) => state.screen);
+  // console.log("isMobileScreen =>", isMobileScreen);
+
+  const [isMobileScreen] = useAtom(isMobileScreenAtom);
   console.log("isMobileScreen =>", isMobileScreen);
 
   // fungsi untuk memperbarui nilai state
